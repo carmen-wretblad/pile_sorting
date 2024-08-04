170190c4b016e84eefb48b8147b5da1c74a5692f
Carmen <carmen.wretblad@gmail.com>
2024-08-04 13:48:10 +0200

    program created

---
+++ src/program.rs
@@ -0,0 +1,51 @@
+#![allow(unused)]
+// ### Questions ###
+// Is there a trait for structs that can be created with New?
+// Answered: What happens when a struct is moved: Depends
+// If a struct has a field, is the field moved "with it?"
+// -------------------------------------------------------
+// Instresting perspecitve: you can see this as a single state machine and you want to find the
+// smallest amount of signals to get it to move from one state to another
+// ### TODO ###
+// Implement "Solution"
+type Move = [u8; 2];
+pub struct Board {}
+struct Info {}
+
+pub trait Program {
+    fn starting_state(&self) -> &Board;
+    fn done(&self) -> bool;
+    /// Runs the program until a new Move has been reached, must change result of done method when
+    /// applicable
+    fn step(&mut self) -> Option<Move>;
+    /// Runs the program to completion.
+    /// Will return new moves made only
+    fn run(&mut self) -> Vec<Move> {
+        let mut vec: Vec<Move> = Vec::<Move>::new();
+        while (!self.done()) {
+            if let Some(value) = self.step() {
+                vec.push(value)
+            }
+        }
+        vec
+    }
+    /// Returns all moves so far
+    fn progress(&self) -> &Vec<Move>;
+}
+/// stuff all programs should contain
+pub struct BFS {
+    name: String,
+    starting_board: Board,
+    moves: Vec<Move>,
+    done_flag: bool,
+}
+impl BFS {
+    fn new(name: String, starting_board: Board) -> Self {
+        BFS {
+            name,
+            starting_board,
+            moves: Vec::<Move>::new(),
+            done_flag: unimplemented!(),
+        }
+    }
+}
