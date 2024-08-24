69d610133145b14ea2c98a62ba2c98e4c2806631
Carmen <carmen.wretblad@gmail.com>
2024-08-24 15:47:57 +0200

    added sorting for mutiple piles

--- src/sortedness.rs
+++ src/sortedness.rs
@@ -1,4 +1,5 @@
 use std::iter::Sum;
+use std::usize;
 
 use crate::board::Board;
 use crate::BoardRep;
@@ -15,8 +16,14 @@ pub fn sortedness_vector(vector: &[u8]) -> usize {
     sign_changes.retain(|w| *w);
     sign_changes.len()
 }
+fn sortedness_vector_list(vectors: &[Vec<u8>]) -> usize {
+    let mut agg = 0;
+    for vec in vectors {
+        agg += sortedness_vector(vec);
+    }
+    agg
+}
 
-#[cfg(test)]
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -27,5 +34,13 @@ mod tests {
         assert_eq!(sortedness_vector(&[2, 3, 1]), 1);
         assert_eq!(sortedness_vector(&[1, 3, 2]), 1);
         assert_eq!(sortedness_vector(&[5, 4, 6, 2, 3, 1]), 4);
+        assert_eq!(
+            sortedness_vector_list(&[
+                [3, 2, 1].to_vec(),
+                [2, 3, 1].to_vec(),
+                [5, 4, 6, 2, 3, 1].to_vec()
+            ]),
+            5
+        );
     }
 }
